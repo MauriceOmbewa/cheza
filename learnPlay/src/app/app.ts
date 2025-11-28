dbdff38efdee4e36c72767c60a50acbc0452f65d
Maurice <ombewamaurice79@gmail.com>
2025-11-28 20:34:45 +0300

    Update app component configuration

--- learnPlay/src/app/app.ts
+++ learnPlay/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // import { LandingPage } from './landing-page-module/components/landing-page/landing-page';
 import { LandingPageComponent } from './landing-page-module/components/landing-page-component/landing-page-component';
+import { WhoAreYouComponent } from './landing-page-module/components/who-are-you-component/who-are-you-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, WhoAreYouComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
