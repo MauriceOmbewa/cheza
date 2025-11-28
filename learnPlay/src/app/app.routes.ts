9f9a5adfc255f6db45e486ba135fce11ceaf29ae
Maurice <ombewamaurice79@gmail.com>
2025-11-28 20:34:34 +0300

    Add routing configuration for landing page and who-are-you components

--- learnPlay/src/app/app.routes.ts
+++ learnPlay/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page-module/components/landing-page-component/landing-page-component';
+import { WhoAreYouComponent } from './landing-page-module/components/who-are-you-component/who-are-you-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPageComponent
+    },
+    {
+        path: 'who-are-you',
+        component: WhoAreYouComponent
+    }
+];
